401e27a463fc68da6552aa5f484bd3afc386c7f9
Suhail Fraj <esootv2@gmail.com>
2025-04-29 23:44:42 +0100

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,76 @@
+def print_board(board):
+    for i in range(9):
+        for j in range(9):
+            print(board[i][j], end=" ")
+        print()
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check row
+    for i in range(9):
+        if board[row][i] == num and col != i:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num and row != i:
+            return False
+
+    # Check box
+    box_x = col // 3
+    box_y = row // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True  # Solved
+    else:
+        row, col = find
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+# Sample puzzle (0 represents empty cells)
+sudoku_board = [
+    [5, 1, 7, 6, 0, 0, 0, 3, 4],
+    [2, 8, 9, 0, 0, 4, 0, 0, 0],
+    [3, 4, 6, 2, 0, 5, 0, 9, 0],
+    [6, 0, 2, 0, 0, 0, 0, 1, 0],
+    [0, 3, 8, 0, 0, 6, 0, 4, 7],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 0, 7, 8],
+    [7, 0, 3, 4, 0, 0, 5, 6, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+print("Original Sudoku:")
+print_board(sudoku_board)
+
+if solve(sudoku_board):
+    print("\nSolved Sudoku:")
+    print_board(sudoku_board)
+else:
+    print("No solution exists.")
