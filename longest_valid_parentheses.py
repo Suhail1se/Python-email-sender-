543bd568546e2380f80ec2c2d9cd124485e27522
Suhail Fraj <esootv2@gmail.com>
2025-05-05 15:01:17 +0100

    Create longest_valid_parentheses.py

---
+++ longest_valid_parentheses.py
@@ -0,0 +1,17 @@
+def longestValidParentheses(s):
+    stack = [-1]
+    max_len = 0
+    for i, char in enumerate(s):
+        if char == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+            if not stack:
+                stack.append(i)
+            else:
+                max_len = max(max_len, i - stack[-1])
+    return max_len
+
+if __name__ == "__main__":
+    test_string = "(()())())(()((())"
+    print(longestValidParentheses(test_string))
